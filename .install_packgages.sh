7674784f4e7abd38136339501b66c32072b281d9
Thales Tozatto <ttozatto@gmail.com>
2024-06-01 15:39:33 -0300

    completes instalation script

--- .install_packgages.sh
+++ .install_packgages.sh
@@ -3,14 +3,27 @@
 # Atualiza a lista de pacotes
 sudo pacman -Syu
 
+# Instala Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Instala nushell
+brew install nushell
+
+# Instala Oh-my-posh
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+
 # Instalação de pacotes do Arch Linux
-sudo pacman -S alacritty nushell neovim ripgrep gcc make fzf bat
+yay alacritty neovim ripgrep gcc make fzf bat
+
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
 
 # Instalação do pipx e virtualenv
 sudo pacman -S python-pipx python-virtualenv
 
-# Instalação do Oh My Posh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyposh/ohmyposh/master/tools/install.sh)"
+cd .config
+cp alacritty ~/.config --recursive
+cp nushell ~/.config --recursive
+cp nvim ~/.config --recursive
 
 echo "Todos os pacotes foram instalados com sucesso!"
 
