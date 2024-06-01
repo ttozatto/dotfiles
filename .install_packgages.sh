728e581485e3f49125e00f1b152cfba3451ed8c5
Thales Tozatto <ttozatto@gmail.com>
2024-06-01 16:27:51 -0300

    fix instalation script

--- .install_packgages.sh
+++ .install_packgages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Atualiza a lista de pacotes
-pacman -Syu
+sudo pacman -Syu
 
 # Instala Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -12,22 +12,23 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # Instala nushell
 brew install nushell
 
+# Intalação de pacotes
+yes | sudo pacman -S yay alacritty neovim ripgrep make \ 
+  fzf bat python-pipx neofetch 
+
+# Instala gcc
+yay gcc
+
 # Instala Oh-my-posh
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
-
-# Intala yay
-pacman -S yay
-
-# Instalação de pacotes do Arch Linux
-yay alacritty neovim ripgrep gcc make fzf bat python-pipx neofetch
 
 # Instalação do virtualenv
 pipx install virtualenv
 
 cd .config
-cp -rf alacritty ~/.config
-cp -rf nushell ~/.config
-cp -rf nvim ~/.config
+yes | cp -rf alacritty ~/.config
+yes | cp -rf nushell ~/.config
+yes | cp -rf nvim ~/.config
 
 echo "Todos os pacotes foram instalados com sucesso!"
 
