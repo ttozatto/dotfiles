2d77c1616dda765d4ebc5d6522fcb4b58f76e9ec
Thales Tozatto <ttozatto@gmail.com>
2024-06-01 16:05:32 -0300

    fix instalation script

--- .install_packgages.sh
+++ .install_packgages.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Atualiza a lista de pacotes
-sudo pacman -Syu
+pacman -Syu
 
 # Instala Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Instala nushell
 brew install nushell
 
 # Instala Oh-my-posh
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
+
+# Intala yay
+pacman -S yay
 
 # Instalação de pacotes do Arch Linux
 yay alacritty neovim ripgrep gcc make fzf bat python-pipx neofetch
